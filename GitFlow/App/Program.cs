746d098e37f7c140ed49300a7c1d25bb18d15c5a
Tej <arpit.khandelwal@propay.com>
2016-12-28 12:15:43 +0530

    Added greeting message to the App

--- GitFlow/App/Program.cs
+++ GitFlow/App/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace App
 {
@@ -10,6 +6,12 @@ namespace App
     {
         static void Main(string[] args)
         {
+            Console.WriteLine();
+            Console.WriteLine("What's your name? ");
+            var name = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("Hello {0}", name);
+            Console.ReadLine();
         }
     }
 }
